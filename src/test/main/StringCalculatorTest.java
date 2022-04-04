4b63f2762be3d7c4df30cccf0ff1f3ed5cb26b82
Mrigesh <006mrigesh@gmail.com>
2022-04-04 18:07:19 +0530

    test added

--- src/test/main/StringCalculatorTest.java
+++ src/test/main/StringCalculatorTest.java
@@ -2,12 +2,20 @@ package main;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class StringCalculatorTest {
 
     @Test
     public void main() {
-        
+
     }
+
+    @Test
+    public void sum() {
+        int result = StringCalculator.sum("10,20");
+        int correct = 30;
+        assertEquals(30,result);
+    }
+    
 }
